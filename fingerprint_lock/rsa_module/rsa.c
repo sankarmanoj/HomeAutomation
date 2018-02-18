bd1cd39bdd32f3e6a223368652a2644014956a66
Madhav Manoj <madhav.manoj@aspecscire.com>
2018-02-18 14:16:40 +0530

    Added library for RSA encryption/decryption

---
+++ fingerprint_lock/rsa_module/rsa.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+#include <RSA-Library/rsa.h>
+
+
+
+int main() {
+    printf("Hello world!");
+    return 0;
+    
+}
